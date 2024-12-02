ae8bda62db4ba2ca4ab74aa45ce08a831291e2e4
orosz.eszter <orosz.eszter@28-DIAK7>
2024-12-02 09:59:21 +0100

    Add project files.

---
+++ dologzat_12.02/Book.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace dologzat_12._02_
+{
+    public class Book
+    {
+        public long ISBN { get; private set; }
+        public List<Author> Authors { get; private set; }
+        public string Title { get; private set; }
+        public int PublicationYear { get; private set; }
+        public string Language { get; private set; }
+        public int Stock { get; set; }
+        public int Price { get; private set; }
+        public Book(long isbn, List<Author> authors, string title, int publicationYear, string language, int price)
+        {
+            if (authors == null || authors.Count < 1 || authors.Count > 3) throw new ArgumentException("nem megfelelő a szerzők száma");
+            if (title.Length < 3 || title.Length > 64) throw new ArgumentException("az érték 3 vagy 64 karakterhosszúság között lehet");
+            if (publicationYear < 2007 || publicationYear > DateTime.Now.Year) throw new ArgumentException("az év 2007 vagy a jelenlegi év lehet");
+            if (language != "angol" && language != "német" && language != "magyar") throw new ArgumentException("a nyelv csak angol, csak német, vagy csak magyar lehet");
+            if (price < 1000 || price > 10000 || price % 100 != 0) throw new ArgumentException("az érték 1000 és 10000 között lehet");
+            ISBN = isbn;
+            Authors = authors;
+            Title = title;
+            PublicationYear = publicationYear;
+            Language = language;
+            Stock = 0;
+            Price = price;
+        }
+        public Book(string title, string authorName) : this(new Random().Next(1000000000, 1000000000),
+            new List<Author> { new Author(authorName) },
+            title,
+            2024,
+            "magyar",
+            4500)
+        { }
+        public override string ToString()
+        {
+            string authorsString = Authors.Count == 1 ? "szerző:" : "szerzők:";
+            string stockString = Stock == 0 ? "beszerzés alatt" : $"{Stock} db";
+            return $"ISBN: {ISBN}\n" + $"{authorsString} " +
+            $"{string.Join(", ", Authors)}\n"
+                + $"cím: {Title}\n"
+                + $"kiadás éve: {PublicationYear}\n"
+                + $"nyelv: {Language}\n"
+                + $"készlet: {stockString}\n"
+                + $"ár: {Price} Ft";
+        }
+    }
+    public class Author
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; }
+        public Guid Id { get; }
+    }
+    public Author(string fullName)
+    {
+        var names = fullName.Split(' ');
+        if (names.Length != 2) throw new ArgumentException("a név egy vezetéknévből és egy keresztnévből kell állnia");
+        if (names[0].Length < 3 || names[0].Length > 32 || names[1].Length < 3 || names[1].Length > 32) throw new ArgumentException("nem megfelelő hosszúságú");
+        
+        FirstName = names[0];
+        LastName = names[1];
+        Id = Guid.NewGuid();
+    }
+    public override string ToString()
+    {
+        return $"{FirstName} {LastName} (ID: {Id})";
+    }
+
+   
+
+
+
+}
---
+++ dologzat_12.02/Program.cs
@@ -0,0 +1,102 @@
+﻿using dologzat_12._02_;
+using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main()
+    {
+        List<Book> books = GenerateRandomBooks();
+        int kezdetiKeszletSzam = GetTotalStockCount(books);
+
+        int eladottKonyvekSzama = 0;
+        int elfogyottKeszletszam = 0;
+        int bevetel = 0;
+
+        for (int i = 0; i < 100; i++)
+        {
+            Book book = SelectRandomBook(books);
+            if (book.Stock > 0)
+            {
+                book.Stock--;
+                bevetel += book.Price;
+                eladottKonyvekSzama++;
+            }
+            else
+            {
+                if (new Random().Next(2) == 0)
+                {
+                    book.Stock = new Random().Next(1, 11);
+                }
+                else
+                {
+                    books.Remove(book);
+                    elfogyottKeszletszam++;
+                }
+            }
+        }
+
+        int jelenlegiKeszletSzam = GetTotalStockCount(books);
+
+        Console.WriteLine($"Összes eladott könyv: {eladottKonyvekSzama}");
+        Console.WriteLine($"Nagykerből elfogyott könyvek: {elfogyottKeszletszam}");
+        Console.WriteLine($"Kezdeti készlet: {kezdetiKeszletSzam} db");
+        Console.WriteLine($"Jelenlegi készlet: {jelenlegiKeszletSzam} db");
+        Console.WriteLine($"Készlet változás: {jelenlegiKeszletSzam - kezdetiKeszletSzam} db");
+        Console.WriteLine($"Bruttó bevétel: {bevetel} Ft");
+    }
+
+    private static List<Book> GenerateRandomBooks()
+    {
+        List<Book> books = new List<Book>();
+        for (int i = 0; i < 15; i++)
+        {
+            string title = $"Cím {i + 1}";
+            List<Author> authors = GenerateRandomAuthors();
+            long isbn = GenerateUniqueISBN(books);
+            string language = new Random().NextDouble() < 0.8 ? "magyar" : "angol";
+            int publicationYear = new Random().Next(2007, DateTime.Now.Year + 1);
+            int price = new Random().Next(1000, 10001) / 100 * 100;
+            books.Add(new Book(isbn, authors, title, publicationYear, language, price));
+        }
+        return books;
+    }
+
+    private static List<Author> GenerateRandomAuthors()
+    {
+        int authorCount = new Random().Next(1, 4);
+        List<Author> authors = new List<Author>();
+        for (int i = 0; i < authorCount; i++)
+        {
+            authors.Add(new Author($"Szerző{i + 1} Vezetéknév"));
+        }
+        return authors;
+    }
+
+    private static long GenerateUniqueISBN(List<Book> books)
+    {
+        long isbn;
+        Random random = new Random();
+        do
+        {
+            isbn = random.Next(1000000000, 10000000000);
+        } 
+        while (books.Exists(book => book.ISBN == isbn));
+        return isbn;
+    }
+
+    private static Book SelectRandomBook(List<Book> books)
+    {
+        return books[new Random().Next(books.Count)];
+    }
+
+    private static int GetTotalStockCount(List<Book> books)
+    {
+        int totalStock = 0;
+        foreach (var book in books)
+        {
+            totalStock += book.Stock;
+        }
+        return totalStock;
+    }
+}
